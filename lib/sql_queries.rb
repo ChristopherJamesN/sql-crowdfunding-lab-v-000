f827183ab26797d7bf2f4d042b2e5aca32deca7d
Chris Nady <nady.christopher@gmail.com>
2017-09-15 22:20:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,7 +46,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "SELECT projects.category, pledges.amount
 FROM pledges
 INNER JOIN projects
-ON pledges.project_id = projects.id 
+ON pledges.project_id = projects.id
 GROUP BY amount
 HAVING category == 'music';"
 end
