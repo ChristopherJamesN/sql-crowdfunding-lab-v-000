0bf0425376c2ed1e2fcd45a27442caf71d9475c5
Chris Nady <nady.christopher@gmail.com>
2017-09-15 22:16:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -48,7 +48,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
-HAVING category == "music";"
+HAVING category == music;"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
@@ -56,5 +56,5 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
 FROM projects
 INNER JOIN pledges
 ON projects.id = pledges.project_id
-HAVING category == "books";"
+HAVING category == books;"
 end
